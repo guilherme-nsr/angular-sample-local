dc84bb609696476faee9c0065d8fba2f268318e2
Guilherme Ribeiro <desaribeiro.guilherme@gmail.com>
2022-07-27 14:51:54 -0300

    feat: format BRL currency

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -8,6 +8,10 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import localePt from '@angular/common/locales/pt'
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localePt, 'pt');
 
 @NgModule({
   imports: [
@@ -27,7 +31,13 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
   ],
   bootstrap: [
     AppComponent
-  ]
+  ],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'pt-BR',
+    }
+  ],
 })
 export class AppModule { }
 
